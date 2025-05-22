53b7d942f75634235a299dda876ccec4d993ba8c
jhfidelis <fidelis.henrique.jh@gmail.com>
2025-05-21 21:20:53 -0300

    Adds loop for continuous program execution

--- src/main/Main.java
+++ src/main/Main.java
@@ -17,52 +17,58 @@ public class Main {
                 1 - Peso argentino (ARS)   =>  Dólar americano (USD)
                 2 - Dólar americano (USD)  =>  Peso argentino (ARS)
                 3 - Real brasileiro (BRL)  =>  Dólar americano (USD)
-                4 - Dólar americano (USD)  =>  Dólar americano (USD)
+                4 - Dólar americano (USD)  =>  Real brasileiro (BRL)
                 5 - Real brasileiro (BRL)  =>  Euro (EUR)
                 6 -            Euro (EUR)  =>  Real brasileiro (BRL)
                 7 - Dólar americano (USD)  =>  Euro (EUR)
                 8 -            Euro (EUR)  =>  Dólar americano (USD)
+                0 - Sair
                 """;
 
-        System.out.println(menu);
+        int opc = 1;
+        while (opc != 0) {
+            System.out.println(menu);
 
-        System.out.print("Escolha a sua opção: ");
-        int opc = sc.nextInt();
+            System.out.print("Escolha a sua opção: ");
+            opc = sc.nextInt();
 
-        System.out.print("Insira o valor a ser convertido: ");
-        double amount = sc.nextDouble();
-
-        ExchangeRateConsultation consultation = new ExchangeRateConsultation();
-
-        String baseCode = "";
-        String targetCurrency = "";
-
-        try {
-            switch (opc) {
-                case 1 -> {baseCode = "ARS"; targetCurrency = "USD";}
-                case 2 -> {baseCode = "USD"; targetCurrency = "ARS";}
-                case 3 -> {baseCode = "BRL"; targetCurrency = "USD";}
-                case 4 -> {baseCode = "USD"; targetCurrency = "BRL";}
-                case 5 -> {baseCode = "BRL"; targetCurrency = "EUR";}
-                case 6 -> {baseCode = "EUR"; targetCurrency = "BRL";}
-                case 7 -> {baseCode = "USD"; targetCurrency = "EUR";}
-                case 8 -> {baseCode = "EUR"; targetCurrency = "USD";}
-                default -> {
-                    System.out.println("Opção inválida!");
-                    return;
-                }
+            if (opc == 0) {
+                break;
             }
 
-            Conversion conversion = consultation.convertCurrency(baseCode);
+            ExchangeRateConsultation consultation = new ExchangeRateConsultation();
 
-            double rate = conversion.conversionRates().get(targetCurrency);
-            double result = CurrencyConverter.convert(amount, rate);
+            String baseCode, targetCurrency;
 
-            System.out.printf("\n$ %.2f %s é igual a $ %.2f %s", amount, baseCode, result, targetCurrency);
-        } catch (RuntimeException e) {
-            System.out.println(e.getMessage());
+            try {
+                switch (opc) {
+                    case 1 -> {baseCode = "ARS"; targetCurrency = "USD";}
+                    case 2 -> {baseCode = "USD"; targetCurrency = "ARS";}
+                    case 3 -> {baseCode = "BRL"; targetCurrency = "USD";}
+                    case 4 -> {baseCode = "USD"; targetCurrency = "BRL";}
+                    case 5 -> {baseCode = "BRL"; targetCurrency = "EUR";}
+                    case 6 -> {baseCode = "EUR"; targetCurrency = "BRL";}
+                    case 7 -> {baseCode = "USD"; targetCurrency = "EUR";}
+                    case 8 -> {baseCode = "EUR"; targetCurrency = "USD";}
+                    default -> {
+                        System.out.println("Opção inválida!");
+                        continue;
+                    }
+                }
+
+                System.out.print("Insira o valor a ser convertido: ");
+                double amount = sc.nextDouble();
+
+                Conversion conversion = consultation.convertCurrency(baseCode);
+
+                double rate = conversion.conversionRates().get(targetCurrency);
+                double result = CurrencyConverter.convert(amount, rate);
+
+                System.out.printf("\n$ %.2f %s é igual a $ %.2f %s\n", amount, baseCode, result, targetCurrency);
+            } catch (RuntimeException e) {
+                System.out.println(e.getMessage());
+            }
         }
-
         sc.close();
     }
 }
